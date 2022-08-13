2815fe964264824a029e1edc93de837888af4c31
Hamid <haha.hamid.haha1380@gmail.com>
2022-08-13 10:10:01 +0430

    add runapp & add contstants

---
+++ lib/constants/contstans.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kBackGroundColor = Color(0xFF040404);
+const Color kTextColor = Color(0xFF80807D);
+const Color kItemColor = Color(0xFFEDEDED);
+const Color kGreenColor = Color(0xFF238B46);
+const Color kCardColor = Color(0xFF333335);
